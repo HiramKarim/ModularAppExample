47e2ac8587dd6c7edd564ff75845aff9436b56ba
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-10-05 22:39:21 -0700

    adding new workspace
    adding new workspace for modular apps

---
+++ MyMainAppModular/MyMainAppModular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMainAppModular
+//
+//  Created by Hiram Castro on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
